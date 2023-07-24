fbd15e97a5da98337edc5288feb7b5133ab0289b
AnnishaSiddika <annisha2112@gmail.comm>
2023-07-24 17:18:31 +0600

    navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import icon from '/collegeIcon.png'
+const Navbar = () => {
+    return (
+      <nav className="bg-green-600 text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+            <div className='flex justify-center items-center gap-2'>
+            <img src={icon} alt="" className='w-12' />
+            <div className="text-xl font-bold">College Booking Pro</div>
+            </div>
+          <div className="hidden md:flex space-x-4">
+            <a href="/" className="hover:text-gray-300">Home</a>
+            <a href="/colleges" className="hover:text-gray-300">Colleges</a>
+            <a href="/admission" className="hover:text-gray-300">Admission</a>
+            <a href="/mycollege" className="hover:text-gray-300">My College</a>
+          </div>
+          <div className="md:hidden flex space-x-4">
+            {/* Mobile Menu */}
+            <button className="focus:outline-none">
+              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
